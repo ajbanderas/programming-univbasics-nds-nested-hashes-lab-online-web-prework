23bbc736bb8053bd6b5e192a47cb3ad0c226073d
Kevin Downs <kevin.downs04@gmail.com>
2019-10-23 19:20:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,8 @@ def assembled_aoh
 end
 
 def literal_aoh
-  array = [DON_G.values.sort, JOELLE_VD.values.sort, PAT_M.values.sort, KATE_G.values.sort, BRUCE_G.values.sort]
+  array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  array.sort
 end
 
 def aoh_lookup(aoh, row, key)
